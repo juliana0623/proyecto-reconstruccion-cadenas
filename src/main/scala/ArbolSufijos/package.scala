b3a04bce7deaadc77f8c74963b212a5cddd38911
Maria Juliana <mjsaatorres@gmail.com>
2025-05-27 00:19:55 -0500

    Initial commit

--- src/main/scala/ArbolSufijos/package.scala
+++ src/main/scala/ArbolSufijos/package.scala
@@ -1,56 +1,116 @@
+import scala.annotation.tailrec
+
 package object ArbolSufijos {
 
-  // Definiendo la estructura para los tries (árboles de prefijos/sufijos)
   abstract class Trie
   case class Nodo(car: Char, marcada: Boolean, hijos: List[Trie]) extends Trie
   case class Hoja(car: Char, marcada: Boolean) extends Trie
 
-  /**
-   * Devuelve el carácter almacenado en la raíz de un Trie.
-   * Para un Nodo, es el carácter del nodo.
-   * Para una Hoja, es el carácter de la hoja.
-   */
   def raiz(t: Trie): Char = {
     t match {
       case Nodo(c, _, _) => c
-      case Hoja(c, _) => c
+      case Hoja(c, _)    => c
     }
   }
 
-  /**
-   * Devuelve una secuencia de caracteres representando las cabezas de los hijos directos de un Nodo.
-   * Para una Hoja, devuelve una secuencia con su propio carácter.
-   */
   def cabezas(t: Trie): Seq[Char] = {
     t match {
-      case Nodo(_, _, lt) => lt.map(t => raiz(t))
-      case Hoja(c, _) => Seq[Char](c)
+      case Nodo(_, _, hijos) => hijos.map(t=>raiz(t))
+      case Hoja(c, _)        => Seq[Char](c)
     }
   }
 
-  /**
-   * Devuelve true si la secuencia s es reconocida por el trie t, y false si no.
-   */
+  @tailrec
   def pertenece(s: Seq[Char], t: Trie): Boolean = {
-    // Implementación de la función pertenece
-    ??? // Implementar lógica aquí
+    (s, t) match {
+      case (Nil, Nodo(_, marcada, _)) => marcada
+      case (Nil, Hoja(_, marcada))    => marcada
+      case (c +: cs, Nodo(_, _, hijos)) =>
+        val hijosEncontrados = hijos.filter(hijo => raiz(hijo) == c)
+        if (hijosEncontrados.isEmpty) {
+          false
+        } else {
+          pertenece(cs, hijosEncontrados.head)
+        }
+      case (c +: cs, Hoja(car_hoja, _)) =>
+        c == car_hoja && cs.isEmpty
+      case _ => false
+    }
   }
 
-  /**
-   * Adiciona una secuencia s (de uno o más caracteres) a un trie t.
-   * Devuelve el nuevo trie con la secuencia adicionada.
-   */
   def adicionar(s: Seq[Char], t: Trie): Trie = {
-    // Implementación de la función adicionar
-    ??? // Implementar lógica aquí
+    def construirNuevaRama(s_actual_rama: Seq[Char], char_actual_rama: Char): Trie = {
+      s_actual_rama match {
+        case Nil => Hoja(char_actual_rama, true)
+        case d +: ds => Nodo(char_actual_rama, false, List(construirNuevaRama(ds, d)))
+      }
+    }
+
+    (s, t) match {
+      case (Nil, Nodo(car_nodo, _, hijos_nodo)) => Nodo(car_nodo, true, hijos_nodo)
+      case (Nil, Hoja(car_hoja, _))             => Hoja(car_hoja, true)
+
+      case (c +: cs, Nodo(car_nodo, marcada_nodo, hijos_nodo)) =>
+        val (hijosCoincidentes, otrosHijos) = hijos_nodo.partition(hijo => raiz(hijo) == c)
+        val ramaProcesada = if (cabezas(t).contains(c)) {
+          adicionar(cs, hijosCoincidentes.head)
+        } else {
+          construirNuevaRama(cs, c)
+        }
+        Nodo(car_nodo, marcada_nodo, ramaProcesada :: otrosHijos)
+      case (c +: cs, Hoja(car_hoja, marcada_hoja)) =>
+        Nodo(car_hoja, marcada_hoja, List(construirNuevaRama(cs, c)))
+    }
   }
 
-  /**
-   * Dada una secuencia no vacía de secuencias (ss),
-   * devuelve el árbol de sufijos asociado a esas secuencias.
-   */
+  def adicionarr(s: Seq[Char], t: Trie): Trie = {
+
+    def construirRama(s: Seq[Char]): Trie = {
+      s match {
+        case s1 +: Seq() => Hoja(s1, true)
+        case s1 +: ss => Nodo(s1, false, List(construirRama(ss)))
+      }
+    }
+
+    if (pertenece(s, t)) {
+      t
+    } else {
+
+      (s, t) match {
+
+        case (s1 +: ss, Hoja(c, m)) => Nodo(c, m, List(construirRama(s1 +: ss)))
+
+        case (s1 +: ss, Nodo(c, m, hijos)) =>
+          if (cabezas(t).contains(s1)) {
+            val nuevosHijos = hijos.map(hijo =>
+              if (raiz(hijo) == s1) {
+                adicionarr(ss, hijo)
+              }
+              else hijo)
+            Nodo(c, m, nuevosHijos)
+          } else {
+            Nodo(c, m, hijos ++ List(construirRama(s1 +: ss)))
+          }
+
+        case (Seq(), Nodo(c, m, hijos)) => Nodo(c, true, hijos)
+
+        case (Seq(), t) => t
+
+      }
+    }
+  }
+
   def arbolDeSufijos(ss: Seq[Seq[Char]]): Trie = {
-    // Implementación de la función arbolDeSufijos
-    ??? // Implementar lógica aquí
+    @tailrec
+    def insertarSufijos(seqsRestantes: Seq[Seq[Char]], trieAcumulado: Trie): Trie = {
+      seqsRestantes match {
+        case Nil => trieAcumulado
+        case seqActual +: colaSeqs =>
+          val sufijos = seqActual.tails.filter(_.nonEmpty)
+          val trieConSufijosActuales = sufijos.foldLeft(trieAcumulado)((accTrie, suf) => adicionar(suf, accTrie))
+          insertarSufijos(colaSeqs, trieConSufijosActuales)
+      }
+    }
+    insertarSufijos(ss, Nodo('_', false, Nil))
   }
 }
--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -1,5 +1,7 @@
-import ArbolSufijos._ // Importa las definiciones de ArbolSufijos si es necesario
-import Oraculo._     // Importa el tipo Oraculo y la función crearOraculo
+import ArbolSufijos.*
+import Oraculo.*
+
+import scala.annotation.tailrec     // Importa el tipo Oraculo y la función crearOraculo
 
 package object ReconstCadenas {
 
@@ -31,9 +33,28 @@ package object ReconstCadenas {
    * Usa la propiedad de que si s <= S, entonces s1 y s2 también son subsecuencias de S.
    */
   def reconstruirCadenaTurbo(n: Int, o: Oraculo): Seq[Char] = {
-    // Implementación de la función reconstruirCadenaTurbo
-    // Asegurarse que n es potencia de 2 según el enunciado.
-    ??? // Implementar lógica aquí
+    def generarCandidatos(SC: Seq[Seq[Char]]): Seq[Seq[Char]] = for {
+      s1 <- SC
+      s2 <- SC
+    } yield s1 ++ s2
+
+    @tailrec
+    def iterar(k: Int, SC: Seq[Seq[Char]]): Seq[Char] = {
+      if (k > n) Seq()
+      else {
+        val candidatos = generarCandidatos(SC)
+        val SCfiltrado = candidatos.filter(w => o(w))
+        val SClongitudN = SCfiltrado.find(w => w.length == n)
+        if (SClongitudN.nonEmpty) {
+          SClongitudN.head
+        } else {
+          iterar(2 * k, SCfiltrado)
+        }
+      }
+    }
+
+    val SC1: Seq[Seq[Char]] = alfabeto.map(c => Seq(c))
+    iterar(2, SC1)
   }
 
   /**
@@ -44,9 +65,32 @@ package object ReconstCadenas {
    * Usa el filtro para ir más rápido.
    */
   def reconstruirCadenaTurboMejorada(n: Int, o: Oraculo): Seq[Char] = {
-    // Implementación de la función reconstruirCadenaTurboMejorada
-    // Necesitará implementar la función Filtrar auxiliar.
-    ??? // Implementar lógica aquí
+    def filtrar(SC: Seq[Seq[Char]], k: Int): Seq[Seq[Char]] = {
+      val candidatos = for {
+        s1 <- SC
+        s2 <- SC
+        s = s1 ++ s2
+        if (1 until k).forall(i => SC.contains(s.slice(i, i + k)))
+      } yield s
+      candidatos
+    }
+
+    @tailrec
+    def iterar(k: Int, SC: Seq[Seq[Char]]): Seq[Char] = {
+      if (k > n) Seq()
+      else {
+        val candidatos = filtrar(SC, k/2)
+        val SCfiltrado = candidatos.filter(w => o(w))
+        val SClongitudN = SCfiltrado.find(w => w.length == n)
+        if(SClongitudN.nonEmpty){
+          SClongitudN.head
+        }else{
+          iterar(2 * k, SCfiltrado)
+        }
+      }
+    }
+    val SC1: Seq[Seq[Char]] = alfabeto.map(c => Seq(c))
+    iterar(2, SC1)
   }
 
   /**
@@ -57,8 +101,27 @@ package object ReconstCadenas {
    * Usa árboles de sufijos para guardar Seq[Seq[Char]] (SCk) y el filtro modificado.
    */
   def reconstruirCadenaTurboAcelerada(n: Int, o: Oraculo): Seq[Char] = {
-    // Implementación de la función reconstruirCadenaTurboAcelerada
-    // Necesitará usar las funciones del paquete ArbolSufijos.
-    ??? // Implementar lógica aquí
+    def filtrar(SC: Seq[Seq[Char]], k: Int): Seq[Seq[Char]] = {
+      val trie = arbolDeSufijos(SC)
+      val combinaciones = for (s1 <- SC; s2 <- SC) yield s1 ++ s2
+      combinaciones.filter(s => s.sliding(k).forall(sub => pertenece(sub, trie)))
+    }
+
+    @tailrec
+    def iterar(k: Int, SC: Seq[Seq[Char]]): Seq[Char] = {
+      if (k > n) Seq()
+      else {
+        val candidatos = filtrar(SC, k/2)
+        val SCfiltrado = candidatos.filter(w => o(w))
+        val SClongitudN = SCfiltrado.find(w => w.length == n)
+        if(SClongitudN.nonEmpty){
+          SClongitudN.head
+        }else{
+          iterar(2 * k, SCfiltrado)
+        }
+      }
+    }
+    val SC1 = alfabeto.map(c => Seq(c)).filter(o)
+    iterar(2, SC1)
   }
 }
